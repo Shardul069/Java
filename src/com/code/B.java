d50e649752a546f4921aaf96b08983683854aba0
Shardul069 <sharduljadhav8452@gmail.com>
2022-10-29 01:22:14 +0530

    h

--- src/com/code/B.java
+++ src/com/code/B.java
@@ -1,15 +1,15 @@
-package com.java8.colg.exp_10.Main.P1;
+package com.java8.colg.exp_10.Main.P2;
 
-public class A extends Thread {
+public class B extends Thread {
     public void run()
     {
-        int[] arr1 = {1,3,5,7,9};
+        int[] arr2 = {2,4,6,8,10};
         for(int i=0; i<5; i++)
         {
-            System.out.println(arr1[i]);
+            System.out.println(arr2[i]);
             try
             {
-                sleep(1000);
+                sleep(1200);
             }
             catch (Exception e)
             {
